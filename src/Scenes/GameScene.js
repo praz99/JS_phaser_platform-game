bdc905f7aef23702735c1c73a8bed4cd5cf96283
praz <t.prazwal@gmail.com>
2020-11-20 16:59:56 +0100

    fix linter errors

--- src/Scenes/GameScene.js
+++ src/Scenes/GameScene.js
@@ -1,16 +1,16 @@
-import 'phaser';
+import Phaser from 'phaser';
 
 export default class GameScene extends Phaser.Scene {
-  constructor () {
+  constructor() {
     super('Game');
   }
 
-  preload () {
+  preload() {
     // load images
     this.load.image('ph_logo', 'assets/logo.png');
   }
 
-  create () {
+  create() {
     this.add.image(400, 300, 'ph_logo');
   }
-};
+}
