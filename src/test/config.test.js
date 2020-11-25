b11072c985c037e7bd38aeeb889fd36117e1b38d
praz <t.prazwal@gmail.com>
2020-11-25 16:07:36 +0100

    add more tests

--- src/test/config.test.js
+++ src/test/config.test.js
@@ -2,11 +2,11 @@ import * as configOptions from '../Config/config';
 
 it('returns screen width', () => {
   expect(configOptions.gameConfig.width).toBe(1430);
-})
+});
 
 it('returns screen height', () => {
   expect(configOptions.gameConfig.height).toBe(680);
-})
+});
 
 it('returns platform speed', () => {
   expect(configOptions.gameOptions.platformSpeedRange).toStrictEqual([300, 300]);
@@ -31,4 +31,18 @@ it('gives player jumpforce', () => {
 it('limits player jump to 2 times', () => {
   expect(configOptions.gameOptions.jumps).toEqual(2);
   expect(configOptions.gameOptions.jumps).not.toBeLessThan(2);
+});
+
+it('returns platform spawn range', () => {
+  expect(configOptions.gameOptions.spawnRange).toStrictEqual([80, 300]);
+})
+
+it('gives minimum limit to platform spawn rate', () => {
+  expect(configOptions.gameOptions.spawnRange[0]).toBe(80);
+  expect(configOptions.gameOptions.spawnRange[0]).not.toBeLessThan(80);
+});
+
+it('gives maximum limit to platform spawn rate', () => {
+  expect(configOptions.gameOptions.spawnRange[1]).toBe(300);
+  expect(configOptions.gameOptions.spawnRange[1]).not.toBeLessThan(300);
 });
